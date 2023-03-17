cb4936d989bf78df40498e401749c0cf9b80d738
raheemelassadi <raheemelassadi1@gmail.com>
2023-03-16 20:46:07 -0700

    added particle js

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+
+
+function App() {
+
+  return (
+    <div className="bg-blue-400 flex h-screen">
+        <div className='m-auto'>
+          <h1>hello</h1>
+        </div>
+    </div>
+  )
+}
+
+export default App
