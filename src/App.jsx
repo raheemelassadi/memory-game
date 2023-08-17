80e17150cbaed65b36a01b188f156ce33dd55eca
raheemelassadi <raheemelassadi1@gmail.com>
2023-08-16 21:32:58 -0700

    file init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold bg-red-300 cursor-pointer">
+        Memory Game
+      </h1>
+    </>
+  );
+}
+
+export default App;
